e24627836c1af83bcad31fa98cd442591da5acb2
089u97t <maryanthonytochukwu@gmail.com>
2024-07-08 07:55:12 -0700

    yes

--- index.js
+++ index.js
@@ -35,3 +35,34 @@ var swiper = new swiper(".swiper", {
 
 
 document.querySelector("body > div.cs_moving_wrap.background-filled.text-uppercase.text-white.d-flex.align-items-center")
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+  }
+  
+ setInterval(() => {
+    d = new Date();//object of date()
+    hr = d.getHours();
+    min = d.getMinutes();
+    sec = d.getSeconds();
+    hr_rotation = 30 * hr + min / 2;//converting current time
+    min_rotation = 6 * min;
+    sec_rotation  = 6 * sec;
+
+    hour.style.transform = rotation($,{hr_rotation}.deg) ;
+    minute.style.transform = rotation($,{min_rotation}.deg) ;
+    second.style.transform = rotation($,{min_rotation}.deg) ;
+ }, 1000);
